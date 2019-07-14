2cd0843122cc6175826f7edfe80411a4e3ecf265
mjtusuru_tm <mjtsurutomita@gmail.com>
2019-07-14 13:55:30 +0900

    testing

--- server.js
+++ server.js
@@ -1,26 +1,347 @@
+/**
+ * Server App main
+ */
+"use strict";
+
+/*
+*
+*/
+var CAMERA_NAME_1 = 3;
+var CAMERA_NAME_2 = 4;
+
+const URI_SCANNED_BUFFER = "scanned_buffer";
+const URI_SCANNED_IMAGES = "scanned_images";
+const URI_PRINT_BUFFER = "print_buffer";
+const URI_SCAN_LIST = "scan_list";
+const URI_REFRESH_SCAN = "refresh_scan";
+const URI_EXEC_PRINT = "exec_print";
+const LOCAL_SCANNED_BUFFER = URI_SCANNED_BUFFER;
+const LOCAL_SCANNED_IMAGES = URI_SCANNED_IMAGES;
+const LOCAL_PRINT_BUFFER = URI_PRINT_BUFFER;
+
+/*
+* Modules
+*/
+var program = require('commander');
+// for async control
+const async = require('async');
+// for http server
+var HTTP = require( "http" );
+var FS = require( "fs" );
+var glob = require("glob");
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 var app = express();
 var http = require('http').Server(app);
 // for interactive messaging
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 80;
+// for the filesystem monitoring
+var chokidar = require('chokidar');
+// for the filename timestamp
+var date = require('dateformat');
+var now = new Date();
 
-var sharp = require('sharp');
+//for the printout
+const PDFDocument = require('pdfkit');
+const doc = new PDFDocument({autoFirstPage:false});
+var printer = require("printer"),
+pdfpath = require('path');
 
-// sharp('data/test.bmp')
-//   .resize(720, 1115,{
-//     position: 'south'
-//   })
-//   .rotate(90)
-//   .toFile('data/output4.jpg', function(err) {
-//       if (err) console.log(err);
-//   });
+//Command Arguments
+program
+  .option('--cam1 <text>')
+  .option('--cam2 <text>', 'Set uvc camera 2')
+  .parse(process.argv);
+
+var ops = program.opts();
+if (isNaN(parseInt(ops.cam1))) {
+  CAMERA_NAME_1 = ops.cam1;
+} else {
+  CAMERA_NAME_1 = parseInt(ops.cam1);
+}
+if (isNaN(parseInt(ops.cam2))) {
+  CAMERA_NAME_2 = ops.cam2;
+} else {
+  CAMERA_NAME_2 = parseInt(ops.cam2);
+}
+
+//Use chokidar
+var scan_watcher = chokidar.watch(LOCAL_SCANNED_BUFFER, {
+  persistent:true,
+  ignoreInitial:true //without this flag, add events occurs when the watcher is created
+});
+var print_watcher = chokidar.watch(LOCAL_PRINT_BUFFER, {
+  persistent:true,
+  ignoreInitial:true //without this flag, add events occurs when the watcher is created
+});
+
+scan_watcher.on('ready', function() { console.log("start watching " + LOCAL_SCANNED_BUFFER); })
+	.on('add', function(path) { console.log("added file-> " + path); })
+	.on('addDir', function(path) { console.log("added dir-> " + path); })
+	.on('unlink', function(path) { console.log("removed file-> " + path); })
+	.on('unlinkDir', function(path) { console.log("removed dir-> " + path); })
+	.on('change', function(path) { console.log("modified-> " + path); })
+	.on('error', function(error) { console.log("error-> " + error); });
+
+print_watcher.on('ready', function() { console.log("start watching " + LOCAL_PRINT_BUFFER); })
+	.on('add', function(path) {
+    //[TODO] Create unique file name
+
+    var writeStream = FS.createWriteStream('cvt_pdf.pdf');
+    doc.pipe(writeStream);
+    var img = doc.openImage(path);
+    doc.addPage({size: [img.width, img.height]});
+    doc.image(img, 0, 0);
+    doc.end();
+    writeStream.on('finish', function () {
+      var filename = pdfpath.resolve(process.cwd(), 'cvt_pdf.pdf');
+      console.log('printing file name ' + filename);
+
+      FS.readFile(filename, function(err, data){
+        if(err) {
+          console.error('err:' + err);
+          return;
+        }
+        console.log('data type is: '+typeof(data) + ', is buffer: ' + Buffer.isBuffer(data));
+          // printer.printDirect({
+          //     data: data,
+          //     type: 'PDF',
+          //     success: function(id) {
+          //         console.log('printed with id ' + id);
+          //     },
+          //     error: function(err) {
+          //         console.error('error on printing: ' + err);
+          //     }
+          // })
+      });
+    });
+
+    console.log("added file-> " + path);
+  })
+	.on('addDir', function(path) { console.log("added dir-> " + path); })
+	.on('unlink', function(path) { console.log("removed file-> " + path); })
+	.on('unlinkDir', function(path) { console.log("removed dir-> " + path); })
+	.on('change', function(path) {
+/*
+    var writeStream = FS.createWriteStream('cvt_pdf.pdf');
+    doc.pipe(writeStream);
+    var img = doc.openImage(path);
+    doc.addPage({size: [img.width, img.height]});
+    doc.image(img, 0, 0);
+    doc.end();
+    writeStream.on('finish', function () {
+      var filename = pdfpath.resolve(process.cwd(), 'cvt_pdf.pdf');
+      console.log('printing file name ' + filename);
+
+      FS.readFile(filename, function(err, data){
+        if(err) {
+          console.error('err:' + err);
+          return;
+        }
+        console.log('data type is: '+typeof(data) + ', is buffer: ' + Buffer.isBuffer(data));
+          printer.printDirect({
+              data: data,
+              type: 'PDF',
+              success: function(id) {
+                  console.log('printed with id ' + id);
+              },
+              error: function(err) {
+                  console.error('error on printing: ' + err);
+              }
+          })
+      });
+*/
+    console.log("modified-> " + path); })
+
+	.on('error', function(error) { console.log("error-> " + error); })
+
+
+//server setting
+//filelist
+var scanned_file_list = new Object();
+scanned_file_list.files = [];
+
+var storage = multer.diskStorage({
+  destination: './' + LOCAL_PRINT_BUFFER,
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({storage: storage});
 
 app.use(express.static('.'));
+//To Provide scanned file list
+app.use(express.static(URI_SCAN_LIST));
+//Command URI endpoint
+app.use(express.static(URI_REFRESH_SCAN));
+app.use(express.static(URI_SCANNED_IMAGES));
+app.use(bodyParser.json()) // for parsing application/x-www-form-urlencoded
 
 app.get('/' , function(req, res){
     console.log('get req');
     res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/' + URI_SCAN_LIST , function(req, res){
+    console.log('list request');
+
+    //async series for staging up files from buffer
+    async.series([
+      (cb) => {
+        async.each(scanned_file_list.files, function(i, cb_each) {
+          console.log("staging file: " + i);
+          FS.renameSync(LOCAL_SCANNED_BUFFER + "/" + i, LOCAL_SCANNED_IMAGES + "/" + i);
+          cb_each(null);
+        });
+        cb(null);
+      },
+      (cb) => {
+        console.log("staging finished");
+        res.send(JSON.stringify(scanned_file_list));
+      },
+    ]);
+});
+
+app.get('/' + URI_REFRESH_SCAN, function(req, res) {
+
+  async.series([
+    (cb) => {
+      //remove files
+      async.each(scanned_file_list.files, function(i, cb_each) {
+        console.log("removing file: " + LOCAL_SCANNED_IMAGES + "/" + i);
+        FS.unlink(LOCAL_SCANNED_IMAGES + "/" + i, (err) => {
+          if (err) throw err;
+          cb_each(null);
+        });
+      });
+      cb(null)
+    },
+    (cb) => {
+      //refreshing list
+      scanned_file_list.files = [];
+      res.sendStatus(200);
+    }
+  ])
+});
+
+app.get('/' + URI_EXEC_PRINT, function(req, res) {
+  //Execute printing out files under print Buffer
+  //get file list in the Buffer
+  glob(LOCAL_PRINT_BUFFER + "/*.jpg", function(err, files) {
+    if (err) throw err;
+    console.log(files);
+  })
+
+  // [TODO] Transform jpg to pdf then issue jobs to the printers.
+  // Monitor printer jobs and care
+
+  // response should be sent immediately.
+  // Now responding with Internal Server Error.
+  res.sendStatus(500);
+});
+
+app.post('/' + URI_PRINT_BUFFER,  (req, res)  => {
+    //console.log(req.body);
+    console.log("received post request");
+
+    const base64 = req.body.pad.split(',')[1];
+    const decode = new Buffer.from(base64,'base64');
+    //[TODO] make Unique file name
+    now = new Date();
+    var filename = date(now, 'yyyymmddHHMMssl');
+    FS.writeFile(LOCAL_PRINT_BUFFER + '/' + filename + 'p.jpg', decode, (err) => {
+        if(err){
+            console.log(err)
+        }else{
+            console.log('saved');
+            res.sendStatus(200);
+        }
+    });
+});
+
+//socket.io messaging
+io.on('connection',function(socket){
+    console.log('connected');
+
+    socket.on('msg', function(data, ack) {
+      console.log('received emit');
+      console.log(data);
+      ack('ack for emit');
+    });
+
+    socket.on('message', function(data, ack) {
+      now = new Date();
+      var filename = date(now, 'yyyymmddHHMMssl');
+
+      switch (data) {
+        case 'scan_device1':
+          console.log('device1 scan');
+          Webcam_1.capture( 'tmp1', function( err, data ) {
+              if( err ) {
+                  throw err;
+              }
+              Jimp.read('tmp1.bmp', (err, func) => {
+                if (err) throw err;
+                func
+                  .crop(0, 0, 1115, 720)
+                  .rotate(-90)
+                  .write(LOCAL_SCANNED_BUFFER + "/" + filename + ".jpg", jimpwritecallback(socket, ack, filename, 1));
+              });
+          });
+        break;
+        case 'scan_device2':
+          console.log('device2 scan');
+          Webcam_2.capture('tmp2', function( err, data ) {
+              if( err ) {
+                  throw err;
+              }
+              Jimp.read('tmp2.bmp', (err, func) => {
+                if (err) throw err;
+                func
+                  .crop(0, 0, 1115, 720)
+                  .rotate(-90)
+                  .write(LOCAL_SCANNED_BUFFER + "/" + filename + ".jpg", jimpwritecallback(socket, ack, filename, 2));
+              });
+          });
+        break;
+      }
+      console.log('received send');
+
+    });
+});
+
+//Responding after image processing is finished
+function jimpwritecallback(socket, ack, filename, device) {
+  scanned_file_list.files.push(filename + ".jpg");
+  console.log(JSON.stringify(scanned_file_list));
+  ack('captured');
+  var msg = new Object();
+  msg.device = device;
+  msg.filename = filename + ".jpg";
+  socket.send(JSON.stringify(msg), function onack(res) {
+    console.log(res);
+  });
+}
+
+//Webcam usage
+var NodeWebcam = require( "node-webcam" );
+//image processing
+var Jimp = require("jimp");
+
+var Webcam_1 = NodeWebcam.create({
+    callbackReturn: "base64",
+    saveShots: false,
+    device: CAMERA_NAME_1,
+    output: "bmp",
+});
+
+var Webcam_2 = NodeWebcam.create({
+    callbackReturn: "base64",
+    saveShots: false,
+    device: CAMERA_NAME_2,
+    output: "bmp",
 });
 
 //Start Server
--- sketch.js
+++ sketch.js
@@ -1,5 +1,17 @@
-//CANVAS_HEIGHT = 720;
-//CANVAS_WIDTH = 1280;
+//For Reception
+
+CANVAS_BACK_W = 1920;
+CANVAS_BACK_H = 1080;
+
+SCAN_IMG_W = 720;
+SCAN_IMG_H = 1115;
+SCAN_IMG_DIV = 3.28;
+SCAN_IMG_X_ZERO = 44;
+SCAN_IMG_Y_ZERO = 172;
+SCAN_IMG_X_PADDING = 652;
+
+STATUS_H = 32;
+
 CANVAS_HEIGHT = 372;
 CANVAS_WIDTH = 240;
 CANVAS_UPPER_X = 100;
@@ -7,72 +19,238 @@ CANVAS_LEFT_X = 100;
 CANVAS_LEFTEND_MARGIN = 100;
 CANVAS_MARGIN = 20;
 
-function setup() {
-  //fullscreen(); //MBP 2560 x 1600
-  cnv_1 = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  img = loadImage('data/test.png'); //720 x 1115
-  done = loadImage('data/done.png'); //2237 × 836
-  background(51);
-  cnv_2 = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  img = loadImage('data/test.png'); //720 x 1115
-  done = loadImage('data/done.png'); //2237 × 836
-  background(51);
-  //cnv = createCanvas(100, 100);
-  cnv_1.mouseOver(Selected);
-  cnv_1.mouseOut(Disselected);
+var tint_val_scan1 = 255;
+var tint_val_scan2 = 255;
 
-  //place the canvases
-  cnv_1.position(CANVAS_LEFTEND_MARGIN, CANVAS_UPPER_X);
-  cnv_2.position(CANVAS_LEFTEND_MARGIN + CANVAS_WIDTH + CANVAS_MARGIN, CANVAS_UPPER_X);
-  tint_val = 255;
+var img_scan1_src;
+SCAN1_STATUS_OK = 0;
+SCAN1_STATUS_DOING = 1;
+SCAN1_STATUS_LOADING = 2;
+SCAN1_STATUS_LOADED = 3;
+SCAN1_STATUS_BATSU = 4;
+var scan1_status = SCAN1_STATUS_OK;
+
+var img_scan2_src;
+SCAN2_STATUS_OK = 0;
+SCAN2_STATUS_DOING = 1;
+SCAN2_STATUS_LOADING = 2;
+SCAN2_STATUS_LOADED = 3;
+SCAN2_STATUS_BATSU = 4;
+var scan2_status = SCAN2_STATUS_OK;
+
+
+var socket = io();
+socket.on('message', function(msg, ack) {
+  console.log(msg);
+  var msg = JSON.parse(msg);
+
+  if (msg.device == 1) {
+    if (scan1_status == SCAN1_STATUS_DOING) {
+      img_scan1_src = "scanned_buffer/" + msg.filename;
+      console.log('scan1_status to loading');
+      scan1_status = SCAN1_STATUS_LOADING;
+    }
+  } else if (msg.device == 2) {
+    if (scan2_status == SCAN2_STATUS_DOING) {
+      img_scan2_src = "scanned_buffer/" + msg.filename;
+      console.log('scan2_status to loading');
+      scan2_status = SCAN2_STATUS_LOADING;
+    }
+  }
+
+  ack('client ack for send');
+});
+
+function sketchFuncs(p) {
 
 }
 
-function draw() {
-  tint(tint_val, tint_val);
-  image(img, 0, 0, img.width / 3, img.height / 3);
-  tint(255, 255);
-  image(done, 0, 100, done.width / 10, done.height / 10);
+//var socket = io();
+
+var cnv_back;
+let song;
+var sketchBack = function(p) {
+  img_back = p.loadImage('data/scan_back_g.png');
+  p.setup = function() {
+
+    cnv_back = p.createCanvas(CANVAS_BACK_W / 2, CANVAS_BACK_H / 2);
+    p.background(0);
+    p.image(img_back, 0, 0, CANVAS_BACK_W / 2, CANVAS_BACK_H / 2);
+  };
+  p.draw = function() {
+    p.image(img_back, 0, 0, CANVAS_BACK_W / 2, CANVAS_BACK_H / 2);
+  };
 }
 
-function Selected() {
-  console.log("mouse over");
-  tint_val = 127;
-}
-
-function Disselected() {
-    console.log("mouse out");
-  tint_val = 255;
-}
 
 var sketch1 = function(p) {
-  img = p.loadImage('data/test.png'); //720 x 1115
-  done = p.loadImage('data/done.png'); //2237 × 836
+  img_scan1 = p.loadImage('data/trans.png'); //720 x 1115
+  //done = p.loadImage('data/done.png'); //2237 × 836
   p.setup = function() {
-    p.createCanvas(CANVAS_HEIGHT, CANVAS_WIDTH);
-    p.background(125);
+    cnv = p.createCanvas(SCAN_IMG_W / SCAN_IMG_DIV, SCAN_IMG_H / SCAN_IMG_DIV);
+    p.background(0);
+    cnv.position(SCAN_IMG_X_ZERO, SCAN_IMG_Y_ZERO);
   };
 
   p.draw = function() {
-    p.tint(tint_val, tint_val);
-    p.image(img, 0, 0, img.width / 3, img.height / 3);
-    p.tint(255, 255);
-    p.image(done, 0, 100, done.width / 10, done.height / 10);
+
+    if (scan1_status == SCAN1_STATUS_LOADED) {
+      img_scan1 = p.loadImage(img_scan1_src);
+      scan1_status = SCAN1_STATUS_BATSU;
+    }
+
+    p.image(img_scan1, 0, 0, p.width, p.height);
   };
 };
 
 var sketch2 = function(p) {
-p.setup = function() {
-  p.createCanvas(200, 200);
-  p.background(200);
+  img_scan2 = p.loadImage('data/trans.png'); //720 x 1115
+  //done = p.loadImage('data/done.png'); //2237 × 836
+  p.setup = function() {
+    cnv = p.createCanvas(SCAN_IMG_W / SCAN_IMG_DIV, SCAN_IMG_H / SCAN_IMG_DIV);
+    p.background(0);
+    cnv.position(SCAN_IMG_X_ZERO + SCAN_IMG_X_PADDING, SCAN_IMG_Y_ZERO);
+  };
+
+  p.draw = function() {
+    if (scan2_status == SCAN2_STATUS_LOADED) {
+      img_scan2 = p.loadImage(img_scan2_src);
+      scan2_status = SCAN2_STATUS_BATSU;
+    }
+    p.image(img_scan2, 0, 0, p.width, p.height);
+  };
 };
 
-p.draw = function() {
-  p.fill(0);
-  p.stroke(0);
-  p.ellipse(50, 50, 50, 50);
-};
+var scan1_status_change_done = 0;
+var sketch1_status = function(p) {
+  img_1_status = p.loadImage('data/ok_g.png');
+  song = p.loadSound('data/shinkazoku.wav');
+  p.setup = function() {
+    cnv = p.createCanvas(SCAN_IMG_W / SCAN_IMG_DIV, STATUS_H);
+    p.background(0);
+    cnv.position(SCAN_IMG_X_ZERO, SCAN_IMG_Y_ZERO - STATUS_H);
+    cnv.mouseOver(Status1_Selected);
+    cnv.mouseOut(Status1_Diselected);
+    cnv.mousePressed(OnSendClickDev1);
+  };
+  p.draw = function() {
+    if (scan1_status == SCAN1_STATUS_DOING) {
+      if (scan1_status_change_done == 0) {
+        img_1_status = p.loadImage('data/doing_g.png');
+        scan1_status_change_done = 1;
+      }
+    } else if (scan1_status == SCAN1_STATUS_LOADING) {
+      img_1_status = p.loadImage('data/batsu_g.png');
+      console.log('scan1_status to loaded');
+      scan1_status = SCAN1_STATUS_LOADED;
+      scan1_status_change_done = 0;
+    } else if (scan1_status == SCAN1_STATUS_LOADED) {
+      if (scan1_status_change_done == 0) {
+        console.log('change status img to BATSU');
+        scan1_status_change_done = 1;
+      }
+    }
+
+    p.tint(tint_val_scan1, tint_val_scan1);
+    p.image(img_1_status, 0, 0, p.width, p.height)
+  }
+
+}
+
+function Status1_Selected() {
+  console.log("Status1 mouse over");
+  tint_val_scan1 = 127;
+}
+
+function Status1_Diselected() {
+  console.log("Status1 mouse out");
+  tint_val_scan1 = 255;
+}
+
+function OnSendClickDev1() {
+  console.log("sending dev1 scan msg");
+  scan1_status = SCAN1_STATUS_DOING;
+
+  if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.stop();
+
+  } else {
+    song.play();
+
+  }
+
+  socket.send('scan_device1',function onack(res) {
+    console.log(res);
+  });
 };
 
+var scan2_status_change_done = 0;
+var sketch2_status = function(p) {
+  img_2_status = p.loadImage('data/ok_g.png');
+  song = p.loadSound('data/shinkazoku.wav');
+  p.setup = function() {
+    cnv = p.createCanvas(SCAN_IMG_W / SCAN_IMG_DIV, STATUS_H);
+    p.background(0);
+    cnv.position(SCAN_IMG_X_ZERO + SCAN_IMG_X_PADDING, SCAN_IMG_Y_ZERO - STATUS_H);
+    cnv.mouseOver(Status2_Selected);
+    cnv.mouseOut(Status2_Diselected);
+    cnv.mousePressed(OnSendClickDev2);
+  };
+  p.draw = function() {
+    if (scan2_status == SCAN2_STATUS_DOING) {
+      if (scan2_status_change_done == 0) {
+        img_2_status = p.loadImage('data/doing_g.png');
+        scan2_status_change_done = 1;
+      }
+    } else if (scan2_status == SCAN1_STATUS_LOADING) {
+      img_2_status = p.loadImage('data/batsu_g.png');
+      console.log('scan1_status to loaded');
+      scan2_status = SCAN2_STATUS_LOADED;
+      scan2_status_change_done = 0;
+    } else if (scan2_status == SCAN2_STATUS_LOADED) {
+      if (scan2_status_change_done == 0) {
+        console.log('change status img to BATSU');
+        scan2_status_change_done = 1;
+      }
+    }
+
+    p.tint(tint_val_scan2, tint_val_scan2);
+    p.image(img_2_status, 0, 0, p.width, p.height)
+  }
+}
+
+function Status2_Selected() {
+  console.log("Status2 mouse over");
+  tint_val_scan2 = 127;
+}
+
+function Status2_Diselected() {
+  console.log("Status2 mouse out");
+  tint_val_scan2 = 255;
+}
+
+function OnSendClickDev2() {
+  console.log("sending dev2 scan msg");
+  scan2_status = SCAN2_STATUS_DOING;
+
+  if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.stop();
+
+  } else {
+    song.play();
+
+  }
+
+  socket.send('scan_device2',function onack(res) {
+    console.log(res);
+  });
+};
+
+
+new p5(sketchBack, "container0");
 new p5(sketch1, "container1");
 new p5(sketch2, "container2");
+new p5(sketch1_status, "container3");
+new p5(sketch2_status, "container4");
