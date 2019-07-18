aca207aaa9e38ebde72f41514c9737fc70b0bdf2
tm_tomita <mjtsurutomita@gmail.com>
2019-07-19 02:47:54 +0900

    test

--- reception_sketch.js
+++ reception_sketch.js
@@ -52,13 +52,13 @@ socket.on('message', function(msg, ack) {
 
   if (msg.device == 1) {
     if (scan1_status == SCAN1_STATUS_DOING) {
-      img_scan1_src = "scanned_buffer/" + msg.filename;
+      img_scan1_src = msg.filename;
       console.log('scan1_status to loading');
       scan1_status = SCAN1_STATUS_LOADING;
     }
   } else if (msg.device == 2) {
     if (scan2_status == SCAN2_STATUS_DOING) {
-      img_scan2_src = "scanned_buffer/" + msg.filename;
+      img_scan2_src = msg.filename;
       console.log('scan2_status to loading');
       scan2_status = SCAN2_STATUS_LOADING;
     }
@@ -90,8 +90,10 @@ var sketchBack = function(p) {
 
 
 var sketch1 = function(p) {
-  img_scan1 = p.loadImage('data/trans_y.png'); //720 x 1115
+  img_scan1 = p.loadImage('data/trans_g2.png'); //720 x 1115
   //done = p.loadImage('data/done.png'); //2237 × 836
+  var w = SCAN_IMG_W / SCAN_IMG_DIV;
+  var h = SCAN_IMG_H / SCAN_IMG_DIV;
   p.setup = function() {
     cnv = p.createCanvas(SCAN_IMG_W / SCAN_IMG_DIV, SCAN_IMG_H / SCAN_IMG_DIV);
     //p.background(0);
@@ -99,18 +101,23 @@ var sketch1 = function(p) {
   };
 
   p.draw = function() {
-
-    if (scan1_status == SCAN1_STATUS_LOADED) {
-      img_scan1 = p.loadImage(img_scan1_src);
+    if (scan1_status == SCAN1_STATUS_OK) {
+      if (scan1_status_change_done == 2) {
+        console.log('img scan to transparent');
+        img_scan1 = p.loadImage('data/dummy_g.png');
+        scan1_status_change_done = 0;
+      }
+    } else if (scan1_status == SCAN1_STATUS_LOADED) {
+      img_scan1 = p.loadImage("scanned_buffer/" + img_scan1_src);
       scan1_status = SCAN1_STATUS_BATSU;
     }
 
-    p.image(img_scan1, 0, 0, p.width, p.height);
+    p.image(img_scan1, 0, 0, w, h);
   };
 };
 
 var sketch2 = function(p) {
-  img_scan2 = p.loadImage('data/trans_y.png'); //720 x 1115
+  img_scan2 = p.loadImage('data/trans_g2.png'); //720 x 1115
   //done = p.loadImage('data/done.png'); //2237 × 836
   p.setup = function() {
     cnv = p.createCanvas(SCAN_IMG_W / SCAN_IMG_DIV, SCAN_IMG_H / SCAN_IMG_DIV);
@@ -119,8 +126,11 @@ var sketch2 = function(p) {
   };
 
   p.draw = function() {
+    if (scan2_status == SCAN2_STATUS_OK) {
+      img_scan2 = p.loadImage('data/trans_g2.png');
+    }
     if (scan2_status == SCAN2_STATUS_LOADED) {
-      img_scan2 = p.loadImage(img_scan2_src);
+      img_scan2 = p.loadImage("scanned_buffer/" + img_scan2_src);
       scan2_status = SCAN2_STATUS_BATSU;
     }
     p.image(img_scan2, 0, 0, p.width, p.height);
@@ -140,6 +150,7 @@ var sketch1_status_left = function(p) {
     cnv.mouseOver(Status1_Selected);
     cnv.mouseOut(Status1_Diselected);
     cnv.mousePressed(OnSendClickDev1);
+    p.image(img_1_status_left, 0, 0, img_left_size, p.height);
   };
   p.draw = function() {
     if (scan1_status == SCAN1_STATUS_DOING) {
@@ -151,15 +162,26 @@ var sketch1_status_left = function(p) {
     } else if (scan1_status == SCAN1_STATUS_LOADING) {
       img_left_size = STATUS_W;
       img_1_status_left = p.loadImage('data/batsu_g2.png');
+//      img_1_status_left = p.loadImage('data/next_g.png');
       console.log('scan1_status to loaded');
       scan1_status = SCAN1_STATUS_LOADED;
       scan1_status_change_done = 0;
 
-      img_1_status_right = p.loadImage('data/next_g.png');
+      //img_1_status_right = p.loadImage('data/next_g.png');
     } else if (scan1_status == SCAN1_STATUS_LOADED) {
       if (scan1_status_change_done == 0) {
         console.log('change status img to BATSU');
-        p.image(img_1_status_right, STATUS_W, 0, STATUS_W, STATUS_H);
+        //p.image(img_1_status_right, STATUS_W, 0, STATUS_W, STATUS_H);
+        scan1_status_change_done = 1;
+      }
+    }
+    else if (scan1_status == SCAN1_STATUS_OK) {
+//      console.log('scan1 status default');
+      if (scan1_status_change_done == 0) {
+        console.log('status batsu to ok');
+        img_left_size = STATUS_W * 2;
+        cnv = p.createCanvas(img_left_size, STATUS_H);
+        img_1_status_left = p.loadImage('data/ok_g.png');
         scan1_status_change_done = 1;
       }
     }
@@ -183,63 +205,86 @@ function Status1_Diselected() {
 
 function OnSendClickDev1() {
   console.log("sending dev1 scan msg");
-  scan1_status = SCAN1_STATUS_DOING;
 
-  if (song.isPlaying()) {
-    // .isPlaying() returns a boolean
-    song.stop();
-
+  if (scan1_status == SCAN1_STATUS_BATSU) {
+    socket.emit('rem_buf', img_scan1_src, function onack(res) {
+      console.log(res);
+    });
+    scan1_status_change_done = 3;
+    scan1_status = SCAN1_STATUS_OK;
   } else {
-    song.play();
+    scan1_status = SCAN1_STATUS_DOING;
 
+    if (song.isPlaying()) {
+      // .isPlaying() returns a boolean
+      song.stop();
+
+    } else {
+      song.play();
+
+    }
+
+    socket.send('scan_device1',function onack(res) {
+      console.log(res);
+    });
   }
-
-  socket.send('scan_device1',function onack(res) {
-    console.log(res);
-  });
 };
 
 var sketch1_status_right = function(p) {
   var img_left_size = STATUS_W;
   img_1_status_right = p.loadImage('data/trans_g2.png');
+//    img_1_status_right = p.loadImage('data/next_g.png');
   p.setup = function() {
     cnv = p.createCanvas(img_left_size, STATUS_H);
-    p.background(255);
+    //p.background(0);
     cnv.position(SCAN_IMG_X_ZERO + STATUS_W, SCAN_IMG_Y_ZERO - STATUS_H);
     cnv.mouseOver(Status1Right_Selected);
     cnv.mouseOut(Status1Right_Diselected);
     cnv.mousePressed(OnImageDev1_OK);
+    //p.image(img_1_status_right)
   };
   p.draw = function() {
     if (scan1_status == SCAN1_STATUS_DOING) {
       if (scan1_status_change_done == 0) {
-        img_left_size = STATUS_W * 2;
-        img_1_status_left = p.loadImage('data/doing_g.png');
+        //img_left_size = STATUS_W * 2;
+        //img_1_status_left = p.loadImage('data/doing_g.png');
       }
     } else if (scan1_status == SCAN1_STATUS_LOADING) {
-      img_left_size = STATUS_W;
-      img_1_status_left = p.loadImage('data/batsu_g2.png');
-      console.log('scan1_status to loaded');
-
+      //img_left_size = STATUS_W;
+      //img_1_status_left = p.loadImage('data/batsu_g2.png');
+      console.log('change status1 img to NEXT');
       img_1_status_right = p.loadImage('data/next_g.png');
+      //console.log('scan1_status to loaded');
+    //  p.image(img_1_status_right, 0, 0, STATUS_W, STATUS_H);
     } else if (scan1_status == SCAN1_STATUS_LOADED) {
       if (scan1_status_change_done == 0) {
-        console.log('change status img to BATSU');
-        p.image(img_1_status_right, STATUS_W, 0, STATUS_W, STATUS_H);
+        console.log('change status1 img to NEXT');
+        //p.background(0, 0, 0, 0);
+        img_1_status_right = p.loadImage('data/next_g.png');
+//        p.image(img_1_status_right, STATUS_W, 0, STATUS_W, STATUS_H);
+      }
+    } else if (scan1_status == SCAN1_STATUS_OK) {
+      if (scan1_status_change_done == 3) {
+        console.log('hikitsugi to transparent');
+        //p.background(255, 255, 255, 126);
+        img_1_status_right = p.loadImage('data/trans_g2.png');
+        scan1_status_change_done = 2;
+        //p.image(img_1_status_right, 0, 0, STATUS_W, STATUS_H);
       }
     }
-    p.tint(tint_val_scan1, tint_val_scan1);
-    p.image(img_1_status_right, STATUS_W, 0, STATUS_W, STATUS_H);
+//    p.tint(tint_val_scan1, tint_val_scan1);
+    p.image(img_1_status_right, 0, 0, STATUS_W, STATUS_H);
+
   }
 }
 
 function Status1Right_Selected() {
-  console.log("Status1 mouse over");
+  console.log("Status1Right mouse over");
   tint_val_scan1 = 127;
 }
 
 function Status1Right_Diselected() {
-  console.log("Status1 mouse out");
+  console.log("Status1Right mouse out");
   tint_val_scan1 = 255;
 }
 
@@ -260,8 +305,8 @@ var sketch2_status_left = function(p) {
     cnv = p.createCanvas(SCAN_IMG_W / SCAN_IMG_DIV, STATUS_H);
     p.background(255);
     cnv.position(SCAN_IMG_X_ZERO + SCAN_IMG_X_PADDING, SCAN_IMG_Y_ZERO - STATUS_H);
-    cnv.mouseOver(Status2_Selected);
-    cnv.mouseOut(Status2_Diselected);
+    cnv.mouseOver(Status2Left_Selected);
+    cnv.mouseOut(Status2Left_Diselected);
     cnv.mousePressed(OnSendClickDev2);
   };
   p.draw = function() {
@@ -287,12 +332,12 @@ var sketch2_status_left = function(p) {
   }
 }
 
-function Status2_Selected() {
+function Status2Left_Selected() {
   console.log("Status2 mouse over");
   tint_val_scan2 = 127;
 }
 
-function Status2_Diselected() {
+function Status2Left_Diselected() {
   console.log("Status2 mouse out");
   tint_val_scan2 = 255;
 }
@@ -315,11 +360,72 @@ function OnSendClickDev2() {
   });
 };
 
+var sketch2_status_right = function(p) {
+  var img_left_size = STATUS_W;
+  img_2_status_right = p.loadImage('data/trans_g2.png');
+  p.setup = function() {
+    cnv = p.createCanvas(img_left_size, STATUS_H);
+    //p.background(255);
+    cnv.position(SCAN_IMG_X_ZERO + SCAN_IMG_X_PADDING + STATUS_W, SCAN_IMG_Y_ZERO - STATUS_H);
+    cnv.mouseOver(Status2Right_Selected);
+    cnv.mouseOut(Status2Right_Diselected);
+    cnv.mousePressed(OnImageDev2_OK);
+  };
+  p.draw = function() {
+    if (scan2_status == SCAN2_STATUS_DOING) {
+      if (scan2_status_change_done == 0) {
+        img_left_size = STATUS_W * 2;
+        img_2_status_left = p.loadImage('data/doing_g.png');
+      }
+    } else if (scan2_status == SCAN2_STATUS_LOADING) {
+      img_left_size = STATUS_W;
+      img_2_status_left = p.loadImage('data/batsu_g2.png');
+      console.log('scan1_status to loaded');
+
+      img_2_status_right = p.loadImage('data/next_g.png');
+    } else if (scan2_status == SCAN1_STATUS_LOADED) {
+      if (scan2_status_change_done == 0) {
+        console.log('change status img to BATSU');
+        p.image(img_2_status_right, STATUS_W, 0, STATUS_W, STATUS_H);
+      }
+    }
+    p.tint(tint_val_scan2, tint_val_scan2);
+    p.image(img_2_status_right, STATUS_W, 0, STATUS_W, STATUS_H);
+  }
+}
+function Status2Right_Selected() {
+  console.log("Status2 mouse over");
+  tint_val_scan2 = 127;
+}
+
+function Status2Right_Diselected() {
+  console.log("Status2 mouse out");
+  tint_val_scan2 = 255;
+}
+
+function OnImageDev2_OK() {
+  console.log("sending dev2 scan msg");
+  // scan2_status = SCAN2_STATUS_DOING;
+
+  if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.stop();
+
+  } else {
+    song.play();
+
+  }
+
+  socket.send('scan_device2',function onack(res) {
+    console.log(res);
+  });
+};
+
 
 new p5(sketchBack, "container0");
 new p5(sketch1, "container1");
 new p5(sketch2, "container2");
-new p5(sketch1_status_left, "container3");
-new p5(sketch1_status_right, "container4");
+new p5(sketch1_status_right, "container3");
+new p5(sketch1_status_left, "container4");
 new p5(sketch2_status_left, "container5");
 new p5(sketch2_status_right, "container6")
